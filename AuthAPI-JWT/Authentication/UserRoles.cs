9edfbe121bbb55fe3a3211820c981ed40aa7e5c7
Adam Neujahr <adamneujahr@gmail.com>
2020-09-14 20:56:00 -0500

    Add RegisterModel

---
+++ AuthAPI-JWT/Authentication/UserRoles.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AuthAPI_JWT.Authentication
+{
+    public class UserRoles
+    {
+        public const string Admin = "Admin";
+        public const string User = "User";
+    }
+}
